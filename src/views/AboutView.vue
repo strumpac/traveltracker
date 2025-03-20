c091f6bc7fbf59e289a1aabd985f689ee5568979
rosalinaowo <riccardoromagnoli06@gmail.com>
2025-03-20 11:47:41 +0100

    Skeleton
    Co-authored-by: DiegusFrosty <DiegusFrosty@users.noreply.github.com>
    Co-authored-by: NuveNuvema <NuveNuvema@users.noreply.github.com>
    Co-authored-by: warpear <warpear@users.noreply.github.com>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h2>about works!</h2>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
