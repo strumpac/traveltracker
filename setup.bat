6762b128f21c9592e19cd3c0c9c317ff016525b8
warpear <pirazzinimathias@gmail.com>
2025-05-26 09:46:17 +0200

    Architetture user friendly? Nel mio travel tracker? piu' probabile di quanto tu pensi

--- setup.bat
+++ setup.bat
@@ -1,7 +1,20 @@
 @echo off
-echo Installing node modules...
+echo [0mInstalling node modules...
+timeout /t 1 /nobreak > nul
 CALL npm i
+echo [92mInstalled![0m
+timeout /t 1 /nobreak > nul
 cd backend
+echo Installing backend node modules...
+timeout /t 1 /nobreak > nul
 CALL npm i
+echo [92mInstalled![0m
+timeout /t 1 /nobreak > nul
 cd ..
-echo Run npm run dev in this folder, and node index in the backend folder, to run the program.
+echo Run [93mnpm run dev[0m in this folder to launch the website locally. (Default port: [94m5173[0m)
+timeout /t 1 /nobreak > nul
+echo Run [93mnode index[0m in [94m./backend[0m to start the API server locally.
+timeout /t 2 /nobreak > nul
+echo Thank you for using TravelTracker [31m^<3[0m
+timeout /t 1 /nobreak > nul
+echo - The 5F Team
